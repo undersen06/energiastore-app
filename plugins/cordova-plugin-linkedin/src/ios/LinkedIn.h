36d51cbc1615563136c5e43c4a2d21697580c395
tomas diaz <tomasdiaz@tomass-MacBook-Pro.local>
2018-04-15 22:19:46 -0300

    remove some unnecesary files and folders

--- plugins/cordova-plugin-linkedin/src/ios/LinkedIn.h
+++
@@ -1,9 +0,0 @@
-@interface LinkedIn : CDVPlugin
-- (void) login: (CDVInvokedUrlCommand *) command;
-- (void) logout: (CDVInvokedUrlCommand *) command;
-- (void) getRequest: (CDVInvokedUrlCommand *) command;
-- (void) postRequest: (CDVInvokedUrlCommand *) command;
-- (void) openProfile: (CDVInvokedUrlCommand *) command;
-- (void) hasActiveSession: (CDVInvokedUrlCommand *) command;
-- (void) getActiveSession: (CDVInvokedUrlCommand *) command;
-@end
