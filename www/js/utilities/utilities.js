b175fbd18c269db234568cc6b86f5fb06fa6f502
tomas diaz <tomas.diaz@xintec.cl>
2018-05-27 22:24:25 -0400

    fix login design, wip on sign-up

--- www/js/utilities/utilities.js
+++ www/js/utilities/utilities.js
@@ -2,18 +2,16 @@
 
 (function () {
 	app.factory('Utils', function (StorageUserModel, $rootScope) {
-		var showingToast = false;
+		// var showingToast = false;
 
 		return {
 			validateToast: function (_error) {
 				var message = $rootScope.toast[_error] ||  $rootScope.toast.UNKNOWN_ERROR ;
 
-				if (!showingToast) {
-					showingToast = true;
-					Materialize.toast(message, 4000, '', function () {
-						showingToast = false;
-					});
-				}
+				// if (!showingToast) {
+					// showingToast = true;
+					$.mdtoast(message, { duration: 4000});
+				// }
 			}
 		};
 	});
